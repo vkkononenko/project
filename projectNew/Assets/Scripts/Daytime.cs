909c6b78d4746d903693e5db8ddcbd5584a04346
vkkononenko <konichforever@gmail.com>
2017-09-07 19:25:27 +0300

    Start

---
+++ projectNew/Assets/Scripts/Daytime.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Daytime : MonoBehaviour {
+	
+	// Use this for initialization
+	public Light sun;
+	public float time;
+	bool day;
+	void Start () {
+		day = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (day) {
+			sun.intensity -= Time.deltaTime / time;
+			day = !(sun.intensity == 0);
+		} else {
+			sun.intensity += Time.deltaTime / time;
+			day = (sun.intensity >= 1);
+		}
+		Debug.Log (day);
+	}
+}
---
+++ projectNew/Assets/Scripts/weapon_fire.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class weapon_fire : MonoBehaviour {
+
+	public GameObject fire_position;
+	public GameObject bullet;
+	public int bullet_speed;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButton (0)) {
+			GameObject bullet_fire = (GameObject)Instantiate (bullet, fire_position.transform.position, Quaternion.identity);
+			bullet_fire.GetComponent<Rigidbody> ().AddForce (transform.forward * bullet_speed);
+		}
+	}
+}
